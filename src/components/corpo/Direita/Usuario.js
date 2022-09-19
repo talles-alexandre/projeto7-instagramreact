0215286ac4da05d2a2afead945d07cf7e8c34de2
talles-alexandre <alexandretalles13@gmail.com>
2022-09-19 16:05:19 -0300

    new commit

--- src/components/corpo/Direita/Usuario.js
+++ src/components/corpo/Direita/Usuario.js
@@ -1,13 +1,28 @@
+import React from "react";
 import img_catanacomics from "../../../assets/img/catanacomics.svg";
 export default function Usuario() {
+  const [nomeuser, setNomeuser] = React.useState("Catana");
+  const [img, setImg] = React.useState(
+    "https://st.depositphotos.com/1020341/4233/i/450/depositphotos_42333899-stock-photo-portrait-of-huge-beautiful-male.jpg"
+  );
   return (
     <div class="usuario">
-      <img src={img_catanacomics} />
+      <img
+        src={img}
+        onClick={() => {
+          setImg(prompt("Digite o link da imagem"));
+        }}
+      />
       <div class="texto">
         <strong>catanacomics</strong>
         <span>
-          Catana
-          <ion-icon name="pencil"></ion-icon>
+          {nomeuser}
+          <ion-icon
+            name="pencil"
+            onClick={() => {
+              setNomeuser(prompt("Digite o nome do usuario"));
+            }}
+          ></ion-icon>
         </span>
       </div>
     </div>
